f3474829a4fa5584f840fae8746665bbcf0ee046
MarcoBione <marcobione.work@gmail.com>
2023-05-16 18:00:32 +0200

    List rendering compleate

--- Movie.php
+++ Movie.php
@@ -3,18 +3,18 @@
 class Movie {
 
     public $title;
-    public $category;
+    public $original_title;
     public $date;
     public $votes;
     public $country;
 
     //contructor
-    function __construct($_title, $_category,$_date,$_votes,$_country){
-        $this->title= $_title;
-        $this->category= $_category;
-        $this->date=$_date;
-        $this->votes=$_votes;
-        $this->country=$_country;
+    function __construct($title, $original_title,$date,$votes,$country){
+        $this->title= $title;
+        $this->original_title= $original_title;
+        $this->date=$date;
+        $this->votes=$votes;
+        $this->country=$country;
     }
     
 }
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+include __DIR__.'/Movie.php';
+
+$films = [
+    new Movie ('Il Padrino','The Godfather','1972-03-24','9.2','en'),
+    new Movie ('Via Col Vento','Gone With The Wind','1939-12-15','8.6','en'),
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP | STAMP</title>
+</head>
+<body>
+
+<header>
+    <h1>Lista di film:</h1>
+</header>
+
+<main>
+<ul>
+    <?php foreach ($films as $film): ?>
+        <li>
+            nome : <?php echo $film->title; ?>
+            <br>
+            <?php echo $film->original_title; ?>
+            <br>
+            uscita : <?php echo $film->date; ?>
+            <br>
+            voti: <?php echo $film->votes; ?>
+            <br>
+            lingua :<?php echo $film->country; ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
+</main>
+    
+</body>
+</html>
