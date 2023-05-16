a6651aeb89dcae544f7307783bf7286660e58e35
MarcoBione <marcobione.work@gmail.com>
2023-05-16 16:44:51 +0200

    Define Movie constructor

--- Movie.php
+++ Movie.php
@@ -6,5 +6,15 @@ class Movie {
     public $category;
     public $date;
     public $votes;
+    public $country;
+
+    //contructor
+    function __construct($_title, $_category,$_date,$_votes,$_country){
+        $this->title= $_title;
+        $this->category= $_category;
+        $this->date=$_date;
+        $this->votes=$_votes;
+        $this->country=$_country;
+    }
     
 }
