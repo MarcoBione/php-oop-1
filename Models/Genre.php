923ffd37e2b828cc0517ce507b08daf0a4d85f32
MarcoBione <marcobione.work@gmail.com>
2023-05-17 10:06:49 +0200

    Add Genre folder

---
+++ Models/Genre.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Summary of Genre
+ */
+
+class Gnere{
+    public $name;
+
+    /**
+     * Summary of _construct
+     * @param string $name
+     */
+    public function __construct(string $name){
+        $this->name = $name;
+    }
+}
--- Models/Movie.php
+++ Models/Movie.php
@@ -4,10 +4,10 @@ class Movie {
 
     //metodo corretto per non specificare ad ogni singolo elemento il public
     public 
-    $title,$original_title,$date,$votes,$country;
+    $title,$original_title,$date,$votes,$country, $genre;
 
     //contructor
-    function __construct($title, $original_title,$date,$votes,$country){
+    function __construct(string $title,string $original_title,string $date,int $votes,string $country, Genre $genre){
         $this->title= $title;
         $this->original_title= $original_title;
         $this->date=$date;
