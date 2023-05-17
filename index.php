36e559ea26506777bce610c0e6291b52fdb05d33
MarcoBione <marcobione.work@gmail.com>
2023-05-17 10:23:09 +0200

    Fix @ index - include @ database

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 
-include __DIR__.'/Models/Movie.php';
+include __DIR__.'/Database/database.php';
 
-$films = [
-    new Movie ('Il Padrino','The Godfather','1972-03-24','9.2','en',new Genre('novel')),
-    new Movie ('Via Col Vento','Gone With The Wind','1939-12-15','8.6','en',new Genre('classic')),
-];
 ?>
 
 <!DOCTYPE html>
